5cfb86cd865185a66385ed2718321ba129592d4e
Toby Fox <tobyfox@gmail.com>
2022-05-24 02:16:50 -0600

    chore: Add AccountLinkageCredential

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,10 +1,11 @@
 import { JSONSchema7 } from "json-schema";
 
 import BaseVerifiableCredential from "./BaseVerifiableCredential/latest";
+import AccountLinkageCredential from "./AccountLinkageCredential/latest";
 import KudosCredential from "./KudosCredential/latest";
 import ProfileCredential from "./ProfileCredential/latest";
 
-export { BaseVerifiableCredential, KudosCredential, ProfileCredential };
+export { BaseVerifiableCredential, AccountLinkageCredential, KudosCredential, ProfileCredential };
 
 export const issuableSchemas: JSONSchema7[] = [
   KudosCredential,
--- src/types.ts
+++ src/types.ts
@@ -21,4 +21,3 @@ export interface VC {
     type: string;
   };
 }
-
