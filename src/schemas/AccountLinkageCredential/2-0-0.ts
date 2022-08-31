2f5a701ac1637789dd676313761d07e0cb2b34fc
Denzelle Walker <dwalker@jetlifetech.com>
2022-08-31 17:48:43 -0500

    feat: added account linkage credential v2

---
+++ src/schemas/AccountLinkageCredential/2-0-0.ts
@@ -0,0 +1,46 @@
+import { JSONSchema7 } from "json-schema";
+import BaseVerifiableCredential from "../BaseVerifiableCredential/1-0-0";
+
+export default {
+  ...BaseVerifiableCredential,
+
+  $schema: "http://json-schema.org/draft-07/schema#",
+  $id: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AccountLinkageCredential/2-0-0.json",
+
+  title: "Account Linkage Credential",
+  description: "Credential attesting to the issuer DIDs linkage to the given username on the given service",
+
+  properties: {
+    ...BaseVerifiableCredential.properties,
+
+    credentialSubject: {
+      type: "object",
+      required: ["type", "username"],
+      properties: {
+        type: {
+          type: "string",
+          title: "Type",
+          description: 'Type of account, for instance "Twitter" or "Domain Name"',
+        },
+        username: { type: "string", title: "Username" },
+      },
+    },
+
+    evidence: {
+      type: "object",
+      required: ["id", "type"],
+      properties: {
+        id: {
+          type: "string",
+          format: "uri",
+          description: "Evidence Location"
+        },
+        type: {
+          type: "string",
+          title: "Type",
+          description: 'Type of evidence',
+        }
+      },
+    },
+  },
+} as JSONSchema7;
