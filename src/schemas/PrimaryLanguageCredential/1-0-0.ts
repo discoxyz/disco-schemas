93f2cd6a477888e1953d7bd9efa4fcf6f51c7f72
sruddy <sarah.ruddy@gmail.com>
2022-12-01 15:12:03 -0500

    fix: Remove duplicate language

--- src/schemas/PrimaryLanguageCredential/1-0-0.ts
+++ src/schemas/PrimaryLanguageCredential/1-0-0.ts
@@ -31,7 +31,6 @@ export default {
             "Asturian",
             "Azerbaijani",
             "Basque",
-            "Bulgarian",
             "Belarusian",
             "Bengali",
             "Bosnian",
