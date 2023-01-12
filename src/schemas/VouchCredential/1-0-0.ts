de2b13376fc7b054ef7c00745620d0163b604b1e
Jonny <jonny.howle@gmail.com>
2023-01-12 14:03:50 -0700

    Adding Vouch Credential schema

---
+++ src/schemas/VouchCredential/1-0-0.ts
@@ -0,0 +1,24 @@
+import { JSONSchema7 } from "json-schema";
+import BaseVerifiableCredential from "../BaseVerifiableCredential/1-0-0";
+
+export default {
+  ...BaseVerifiableCredential,
+
+  $schema: "http://json-schema.org/draft-07/schema#",
+  $id: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/VouchCredential/1-0-0.json",
+
+  title: "Vouch Credential",
+  description: "This credential proves the holder has immaculate vibes. The signer vouches for their awesomeness.",
+
+  properties: {
+    ...BaseVerifiableCredential.properties,
+
+    credentialSubject: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { title: "Recipient DID", type: "string", format: "uri" },
+      },
+    },
+  },
+} as JSONSchema7;
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -18,6 +18,7 @@ import PronounsCredential from "./PronounsCredential/latest";
 import SnapsCredential from "./SnapsCredential/latest";
 import TshirtSizeCredential from "./TshirtSizeCredential/latest";
 import StarSignCredential from "./StarSignCredential/latest";
+import VouchCredential from "./VouchCredential/latest"
 
 export {
   AccountLinkageCredential,
@@ -37,7 +38,8 @@ export {
   PronounsCredential,
   SnapsCredential,
   TshirtSizeCredential,
-  StarSignCredential
+  StarSignCredential,
+  VouchCredential
 };
 
 export const issuableSchemas: JSONSchema7[] = [
