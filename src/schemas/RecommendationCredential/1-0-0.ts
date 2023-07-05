0c6cd9cc4c634fbc85dcac76b9a52a54e456bc42
ddinderman <dalton@disco.xyz>
2023-07-05 13:38:01 -0500

    feat: Recommendation credential schema

---
+++ src/schemas/RecommendationCredential/1-0-0.ts
@@ -0,0 +1,35 @@
+import { JSONSchema7 } from "json-schema";
+import BaseVerifiableCredential from "../BaseVerifiableCredential/1-0-0";
+
+export default {
+  ...BaseVerifiableCredential,
+
+  $schema: "http://json-schema.org/draft-07/schema#",
+  $id: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/RecommendationCredential/1-0-0.json",
+
+  title: "Recommendation Credential",
+  description:
+    "Sending over someone who passes the vibe check.",
+
+  properties: {
+    ...BaseVerifiableCredential.properties,
+
+    credentialSubject: {
+      type: "object",
+      required: ["id", "recommendedPerson", "recommendationType"],
+      properties: {
+        id: { title: "Recipient DID", type: "string", format: "uri" },
+        recommendedPerson: { title: "Recommended Person DID", type: "string", format: "uri" },
+        socialHandle: { title: "Recommended Person Social Handle", type: "string" },
+        recommendationType: {
+          type: "string",
+          title: "I think you should",
+          enum: [
+            "Be Friends",
+            "Work Together",
+            "Know About"
+          ],
+        },},
+    },
+  },
+} as JSONSchema7;
